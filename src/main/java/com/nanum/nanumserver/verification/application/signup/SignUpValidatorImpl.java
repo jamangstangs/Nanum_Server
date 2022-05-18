7853820c6a433b5718f0f952b82c10701e785ab3
jamangstangs <jamangsong@gmail.com>
2022-05-18 14:40:27 +0900

    Password, SignUp Validator 구현 완료

--- src/main/java/com/nanum/nanumserver/verification/application/signup/SignUpValidatorImpl.java
+++ src/main/java/com/nanum/nanumserver/verification/application/signup/SignUpValidatorImpl.java
@@ -1,11 +1,33 @@
 package com.nanum.nanumserver.verification.application.signup;
 
+import com.nanum.nanumserver.exception.verification.InvalidVerificationInfoException;
+import com.nanum.nanumserver.exception.verification.NoSuchVerificationCodeException;
+import com.nanum.nanumserver.exception.verification.NotConfirmedVerificationCodeException;
+import com.nanum.nanumserver.verification.domain.SignUpVerificationInfo;
 import com.nanum.nanumserver.verification.domain.SignUpVerificationInfoRepository;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 @Component
 public class SignUpValidatorImpl implements SignUpValidator {
+
     private final SignUpVerificationInfoRepository signUpVerificationInfoRepository;
 
+    public SignUpValidatorImpl(SignUpVerificationInfoRepository signUpVerificationInfoRepository) {
+        this.signUpVerificationInfoRepository = signUpVerificationInfoRepository;
+    }
 
+    @Override
+    public void checkIsVerified(String username, String verificationCode) {
+        SignUpVerificationInfo verificationInfo = signUpVerificationInfoRepository.findByUsernameAndVerificationCode(username, verificationCode)
+                .orElseThrow(NoSuchVerificationCodeException::new);
+        if (!verificationInfo.isConfirmed()) {
+            throw new NotConfirmedVerificationCodeException();
+        }
+
+        if (!verificationInfo.isConfirmationValidAt(LocalDateTime.now())) {
+            throw new InvalidVerificationInfoException();
+        }
+    }
 }
